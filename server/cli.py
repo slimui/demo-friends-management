3e69df9fdfd980b14c7f7ef41b2d1f66f1dd4f28
Ivan Choo <ivanchoo.private@gmail.com>
2017-09-30 00:09:40 +0800

    Add production initdb scripts

--- server/cli.py
+++ server/cli.py
@@ -2,6 +2,9 @@ import click
 import json
 from os import path
 from app.graphql import schema
+from app.models import db
+from app.samples import load_samples
+from app import create_app
 
 
 @click.group(chain=True)
@@ -27,6 +30,22 @@ def graphql(write=True):
             fp.write(output)
     else:
         click.echo(output)
+
+
+@cli.command()
+def initdb():
+    """Initialize database."""
+    click.echo(
+        'Preparing recreate the database, all existing data will be lost.')
+    if not click.confirm('Do you want to continue?'):
+        return
+    app = create_app()
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        load_samples()
+        db.session.commit()
+        click.echo('Done!')
 
 
 if __name__ == '__main__':
