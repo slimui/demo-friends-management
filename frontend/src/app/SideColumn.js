20350ea57cd07a5c5cec5066d47eca27a1b1bee4
Ivan Choo <ivanchoo.private@gmail.com>
2017-09-29 18:27:23 +0800

    Add some explaination to UI. Changed project name. Preparing to upload to github.

--- frontend/src/app/SideColumn.js
+++ frontend/src/app/SideColumn.js
@@ -20,7 +20,7 @@ class SideColumn extends React.Component {
         {...restProps}
         className={`${className} d-flex flex-column bg-light p-3`}
       >
-        <div className="card d-flex flex-column justify-content-around align-items-center">
+        <div className="card d-flex flex-column justify-content-around align-items-center mb-3">
           <UserAvatar url={store.me.avatarUrl} className="mt-3" border />
           <div className="card-body text-center p-2">
             <p className="mb-1">
@@ -41,6 +41,33 @@ class SideColumn extends React.Component {
               })}
             </select>
           </div>
+        </div>
+        <div className="small">
+          <p>
+            You are currently viewing this app as{" "}
+            <strong>{store.me.fullName}</strong>. Click on the avatar to switch
+            identity.
+          </p>
+          <p>
+            An active <span className="icon-check-circle text-success">&nbsp;</span> subscription state means you will receive
+            updates from the user in your news feeds. This is based on the
+            following rules:
+          </p>
+          <ul className="list-unstyled">
+            <li>You have not blocked the user</li>
+            <li>
+              You have at least on of the following:
+              <ul>
+                <li> Are friend of the user</li>
+                <li>Followed the user</li>
+              </ul>
+            </li>
+          </ul>
+          <p>
+            Whenever you befriend or unfriend a user, you will alter the
+            connections between you, the user and your common friends. Try it,
+            and see the changes in your common friends.
+          </p>
         </div>
       </div>
     );
